c0e6810be1a9ae3d87e2b39313b2e5e589e19335
Dylane-JC <56173407+Dylane-JC@users.noreply.github.com>
2021-12-22 22:29:38 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://bf5ca44a.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
